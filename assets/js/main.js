b4de459affa0afee7e62236f19021df07868411a
Jagger81 <eddiejcranmer@gmail.com>
2020-04-23 12:26:31 +0100

    Testing New Accordian Style

--- assets/js/main.js
+++ assets/js/main.js
@@ -197,4 +197,14 @@
     mirror: false
   });
 
+  // accordian plus and minus
+
+  $("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
+    $(e.target)
+      .prev()
+      .find("i:last-child")
+      .toggleClass("fa-minus fa-plus");
+  });
+
+
 })(jQuery);
